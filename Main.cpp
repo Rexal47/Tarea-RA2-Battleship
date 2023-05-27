3ebaacf1e27f8c94960984393bb2390d5430a9e6
Alejandro Torres <ale.torres.jofre@gmail.com>
2023-05-27 13:50:14 -0400

    Separa archivo en dos fuentes

---
+++ Main.cpp
@@ -0,0 +1,43 @@
+#include "Tablero.hpp"
+
+
+//int main(){
+//	//Variable para crear el azar
+//	srand(time(NULL));
+////	for(int i=0;i<100;i++) dale(rand()%10);
+//	Tablero tablita;
+//	char x[1];
+//	int fila, columna;
+//	
+//	int i=0;
+//	while(i<5){
+//		
+//		tablita.Imprimir(true);
+//		tablita.Imprimir(false);
+//		
+//		printf("Ingresa Fila: ");
+//		scanf("%s",x);
+//		printf("Ingresa Columna: ");
+//		scanf("%d",&columna);
+//		
+//		fila = devolverNumero(x[0]);
+////		y = atoi(cord.substr(2,1).c_str()) +1;
+//		
+//		//printf("Coordenadas: %d - %d\n",fila,columna+1);
+//		
+//		tablita.disparo(fila,columna+1);
+//		i++;
+//	}
+//	
+//	
+//	return 0;
+//}
+
+int main(){
+	Tablero cpu, jugador;
+	
+	cpu.Imprimir(true);
+	jugador.Imprimir(false);
+	
+	
+}
--- Tablero.hpp
+++ Tablero.hpp
@@ -4,6 +4,8 @@
 #include <time.h>
 #include <iostream>
 #include <string>
+#ifndef TABLERO_HPP
+#define TABLERO_HPP
 
 using namespace std;
 
@@ -49,10 +51,11 @@ class Tablero{
 	//Valores que indican si el barco esta hundido o no (>0 flotando, =0 hundido)			
 	int portaaviones, buque1, buque2, submarino1, submarino2, lancha;
 	
+	//Funcion que ubica las naves (Largo nave, caracter representativo, pos x, pos y, direccion)
 	bool crearNave(int l, char tipo, int x,int y, int dir){
 		
 		//Dir: 0 Norte, 1 Sur, 2 Oeste, 3 Este
-		//Comprueba que el barco cae en la posiciï¿½n
+		//Comprueba que el barco cae en la posicion
 		if(dir<2){
 			if(dir==0 && x-l<0) {
 				return false;
@@ -69,7 +72,7 @@ class Tablero{
 			}
 		}
 		
-		//Comprueba si hay espacio para poner el barco
+		//Comprueba si hay espacio libre para poner el barco
 		for(int i=0; i<l ; i++) {
 			if(dir<2){
 				if(dir==0 && tRespuestas[x-i][y]!=' ') {
@@ -108,10 +111,10 @@ class Tablero{
 	}
 	
 	//return:
-	// 0 indica que disparó fuera de los límites, 
-	// 1  indica que disparó correctamente, 
-	// -1  indica que ya había disparado a esa posición 
-	int disparo(int x, int y){
+	// 0 indica que disparo fuera de los limites, 
+	// 1  indica que disparo correctamente, 
+	// -1  indica que ya habia disparado a esa posicion 
+	int disparo(int x, int y, bool jugador){ 
 		//Comprueba que el disparo esta dentro de los margenes del tablero
     	if(x<1 || x>15 || y<2 || y>16){
     		printf("Has disparado en una posicion equivocada.\n");
@@ -142,7 +145,7 @@ class Tablero{
 				}
 				tVisible[x][y]=aux;
 			}else{
-				//Comprueba si es un buque
+				//Comprueba si le ha dado a un buque
 				if(aux== 'B' || aux== '8'){
 					if(aux=='B'){
 						buque1--;
@@ -161,7 +164,7 @@ class Tablero{
 					}
 					tVisible[x][y]='B';
 				}else{
-					//Comprueba si es un submarino
+					//Comprueba si le ha dado a un submarino
 					if(aux=='S'){
 						submarino1--;
 						if(submarino1>0) {
@@ -188,9 +191,9 @@ class Tablero{
     	return 1;
   }
 	
-	//imprime en pantalla: F tablero con respuestas, T tablero jugador
-	void Imprimir(bool tipo){
-		if(tipo){
+	//imprime en pantalla: False tablero con respuestas, T tablero jugador
+	void Imprimir(bool visible){
+		if(visible){
 			for(int i=0; i<17;i++){
 				for(int j=0; j<18;j++) printf("%3c", tVisible[i][j]);
 					printf("\n");
@@ -212,65 +215,57 @@ Tablero::Tablero(){
 
 	// crearNave(largo del barco, caracter del barco, eje X, eje Y, direccion del barco)
 	while(!crearNave(5,'P',1+ rand()%15, 2+ rand()%15,rand()%4));
-//    Imprimir();
     while(!crearNave(4,'B',1+ rand()%15, 2+ rand()%15,rand()%4));
-//    Imprimir();
     while(!crearNave(4,'8',1+ rand()%15, 2+ rand()%15,rand()%4));
-//    Imprimir();
     while(!crearNave(3,'S',1+ rand()%15, 2+ rand()%15,rand()%4));
-//    Imprimir();
     while(!crearNave(3,'5',1+ rand()%15, 2+ rand()%15,rand()%4));
-//    Imprimir();
     while(!crearNave(1,'L',1+ rand()%15, 2+ rand()%15,rand()%4));
-//    Imprimir();
     while(!crearNave(1,'L',1+ rand()%15, 2+ rand()%15,rand()%4));
-//    Imprimir();
     while(!crearNave(1,'L',1+ rand()%15, 2+ rand()%15,rand()%4));
-//    Imprimir();
     
+    //Asigna los valores vida de un barco
     portaaviones = 5; 
 	buque1 = 4;
 	buque2 = 4;
 	submarino1 = 3;
 	submarino2 = 3;
+	//Lancha vale 3 porque son 3 barcos de 1 de vida
 	lancha = 3; 
 }
 
-void dale(int a);
-
 int devolverNumero(char letra);
 
-int main(){
-	//Variable para crear el azar
-	srand(time(NULL));
-//	for(int i=0;i<100;i++) dale(rand()%10);
-	Tablero tablita;
-	char x[1];
-	int fila, columna;
-	
-	int i=0;
-	while(i<5){
-		
-		tablita.Imprimir(true);
-		tablita.Imprimir(false);
-		
-		printf("Ingresa Fila: ");
-		scanf("%s",x);
-		printf("Ingresa Columna: ");
-		scanf("%d",&columna);
-		
-		fila = devolverNumero(x[0]);
-//		y = atoi(cord.substr(2,1).c_str()) +1;
-		
-		//printf("Coordenadas: %d - %d\n",fila,columna+1);
-		
-		tablita.disparo(fila,columna+1);
-		i++;
-	}
-	
-	
-	return 0;
-}
+//int main(){
+//	//Variable para crear el azar
+//	srand(time(NULL));
+////	for(int i=0;i<100;i++) dale(rand()%10);
+//	Tablero tablita;
+//	char x[1];
+//	int fila, columna;
+//	
+//	int i=0;
+//	while(i<5){
+//		
+//		tablita.Imprimir(true);
+//		tablita.Imprimir(false);
+//		
+//		printf("Ingresa Fila: ");
+//		scanf("%s",x);
+//		printf("Ingresa Columna: ");
+//		scanf("%d",&columna);
+//		
+//		fila = devolverNumero(x[0]);
+////		y = atoi(cord.substr(2,1).c_str()) +1;
+//		
+//		//printf("Coordenadas: %d - %d\n",fila,columna+1);
+//		
+//		tablita.disparo(fila,columna+1);
+//		i++;
+//	}
+//	
+//	
+//	return 0;
+//}
 
 int devolverNumero(char letra){
 	
@@ -327,3 +322,11 @@ int devolverNumero(char letra){
 	}
 	return numero;
 }
+
+#endif
+
+/*Falta -> f(x) ganar
+si es disparo CPU, no mostrar print? o mostrar quien dispara
+validar disparos (dividir por - en letra y número) = letra no leida error/letra leida buena / numero weno, numero malo, f(x) valida :)
+
+*/

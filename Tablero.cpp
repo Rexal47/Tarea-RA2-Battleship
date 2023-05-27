96ea7b187d4eec20b6f3b6432ec9588db561b327
Alejandro Torres <ale.torres.jofre@gmail.com>
2023-05-26 20:45:31 -0400

    Añade funciones y corrige errores, además de limpiar código

--- Tablero.cpp
+++ Tablero.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 class Tablero{
+	
 	char t[17][18]={{' ','X','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','X'}, 
 					{'A','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
 					{'B','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
@@ -24,127 +25,135 @@ class Tablero{
 					{'N','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
 					{'O','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
 					{' ','X','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','X'}};
-					
-	int intentos;
+	public:				
+	int portaaviones, buque1, buque2, submarino1, submarino2, lancha1, lancha2, lancha3;
 	
-	public:
-	bool crearNave(int l, char tipo){
-		bool flag = true;
-		srand(time(NULL));
-		//x fila, y columna
-		int x = 1+ rand()%15; // 1 ~ 15
-		int y = 2+ rand()%15; // 2 ~ 16
+	bool crearNave(int l, char tipo, int x,int y, int dir){
 		
-		int dir = rand()%4; // 0 Norte, 1 Sur, 2 Oeste, 3 Este
-	
-		switch (dir){
-			case 0:
-				//Comprueba que el barco cae en dicha dirección
-				if(x-l<0) {
-					flag = false;
-				}else{
-					//Comprueba que no hay otro barco
-					for(int i=0; i<l ; i++) {
-						if(t[x-i][y]!='~') flag= false;
-					}
-					if(flag){
-						//Coloca los barcos
-						printf("%d\n",dir);
-						for(int i=0;i<l;i++){
-							t[x-i][y]=tipo;
-						}
-					}
-				}
-				break;
-			case 1:
-				//Comprueba que el barco cae en dicha dirección
-				if(x+l>16) {
-					flag = false;
-				}else{
-					//Comprueba que no hay otro barco
-					for(int i=0; i<l ; i++) {
-						if(t[x+i][y]!='~') flag= false;
-					}
-					if(flag){
-						//Coloca los barcos
-						printf("%d\n",dir);
-						for(int i=0;i<l;i++){
-							t[x+i][y]=tipo;
-						}
-					}
-				}
-				break;
-			case 2:
-				//Comprueba que el barco cae en dicha dirección
-				if(y-l<1) {
-					flag = false;
-				}else{
-					//Comprueba que no hay otro barco
-					for(int i=0; i<l ; i++) {
-						if(t[x][y-i]!='~') flag= false;
-					}
-					if(flag){
-						//Coloca los barcos
-						printf("%d\n",dir);
-						for(int i=0;i<l;i++){
-							t[x][y-i]=tipo;
-						}
-					}
-				}
-				break;
-			case 3:
-				//Comprueba que el barco cae en dicha dirección
-				if(y+l>17) {
-					flag = false;
-				}else{
-					//Comprueba que no hay otro barco
-					for(int i=0; i<l ; i++) {
-						if(t[x][y+i]!='~') flag= false;
-					}
-					if(flag){
-						//Coloca los barcos
-						printf("%d\n",dir);
-						for(int i=0;i<l;i++){
-							t[x][y+i]=tipo;
-						}
-					}
-				}
-				break;
+		//Dir: 0 Norte, 1 Sur, 2 Oeste, 3 Este
+		//Comprueba que el barco cae en la posición
+		if(dir<2){
+			if(dir==0 && x-l<0) {
+				return false;
+			}
+			if(dir==1 && x+l>15) {
+				return false;
+			}
+		}else{
+			if(dir==2 && y-l<1) {
+				return false;
+			}
+			if(dir==3 && y+l>16) {
+				return false;
+			}
 		}
-		return flag;
+		
+		//Comprueba si hay espacio para poner el barco
+		for(int i=0; i<l ; i++) {
+			if(dir<2){
+				if(dir==0 && t[x-i][y]!='~') {
+					return false;
+				}
+				if(dir==1 && t[x+i][y]!='~') {
+					return false;
+				}
+			}else{
+				if(dir==2 && t[x][y-i]!='~') {
+					return false;
+				}
+				if(dir==3 && t[x][y+i]!='~') {
+					return false;
+				}
+			}
+		}
+		
+		//Coloca los barcos
+		for(int i=0; i<l ; i++) {
+			if(dir<2){
+				if(dir==0){
+					t[x-i][y]=tipo;
+				}else{
+					t[x+i][y]=tipo;
+				}
+			}else{
+				if(dir==2 ){
+					t[x][y-i]=tipo;
+				}else{
+					t[x][y+i]=tipo;
+				}
+			}
+		}
+		return true;
 	}
+	
+	char disparo(int x, int y){
+		//Comprueba que el disparo esta dentro de los margenes del tablero
+    	if(x<1 || x>15 || y<2 || y>16) return 0;
+    	char aux = t[x][y];
+    	if(aux!= '~') t[x][y]='X';
+    	return aux;
+  }
 	
 	void Imprimir(){
 	    for(int i=0; i<17;i++){
 			for(int j=0; j<18;j++) printf("%3c", t[i][j]);
 			printf("\n");
 		}
-		printf("        1  2  3  4  5  6  7  8  9  10 11 12 13 14 15");
+		printf("        1  2  3  4  5  6  7  8  9  10 11 12 13 14 15\n\n");
+		system("pause");
  	}
-  
+ 	
 	Tablero();
 };
 
 Tablero::Tablero(){
 	
-	while(!crearNave(5,'5'));
-    //Imprimir();
-    while(!crearNave(4,'4'));
-    while(!crearNave(4,'B'));
-    //Imprimir();
-    while(!crearNave(3,'3'));
-    while(!crearNave(3,'S'));
-    //Imprimir();
-    while(!crearNave(1,'1'));
-    while(!crearNave(1,'1'));
-    while(!crearNave(1,'1'));
+	//Variable para crear el azar
+	srand(time(NULL));
+	
+	// crearNave(largo del barco, caracter del barco, eje X, eje Y, direccion del barco)
+	while(!crearNave(5,'P',1+ rand()%15, 2+ rand()%15,rand()%4));
+//    Imprimir();
+    while(!crearNave(4,'B',1+ rand()%15, 2+ rand()%15,rand()%4));
+//    Imprimir();
+    while(!crearNave(4,'B',1+ rand()%15, 2+ rand()%15,rand()%4));
+//    Imprimir();
+    while(!crearNave(3,'S',1+ rand()%15, 2+ rand()%15,rand()%4));
+//    Imprimir();
+    while(!crearNave(3,'S',1+ rand()%15, 2+ rand()%15,rand()%4));
+//    Imprimir();
+    while(!crearNave(1,'L',1+ rand()%15, 2+ rand()%15,rand()%4));
+//    Imprimir();
+    while(!crearNave(1,'L',1+ rand()%15, 2+ rand()%15,rand()%4));
+//    Imprimir();
+    while(!crearNave(1,'L',1+ rand()%15, 2+ rand()%15,rand()%4));
     Imprimir();
     
-    intentos = 0;
+    portaaviones = 5; 
+	buque1 = 4;
+	buque2 = 4;
+	submarino1 = 3;
+	submarino2 = 3;
+	lancha1 = 1; 
+	lancha2 = 1; 
+	lancha3 = 1;
 }
 
+void dale(int a);
+
 int main(){
+//	srand(time(NULL));
+//	for(int i=0;i<100;i++) dale(rand()%10);
+	Tablero tablita[100];
 	
-	Tablero tablita;
+	
+	tablita[0].Imprimir() ;
+	
+	tablita[1].Imprimir() ;
+	
 	return 0;
+}
+
+void dale(int a){
+	cout << a<<"\n";
 }

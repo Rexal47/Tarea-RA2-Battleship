b0faeef0cd68ccffbb7de814a126bb1d4d771dad
Felipe Vásquez <minelyne@gmail.com>
2023-05-27 22:20:46 -0400

    Preparacion turnos de juego

--- neoCli.cpp
+++ neoCli.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <iostream>
+#include "Tablero.hpp"
 
 // inet_addr
 #include <arpa/inet.h>
@@ -21,26 +22,11 @@ class Client{
 
     std::string mensaje, temp;
     struct sockaddr_in direccion_server;
-    int net_socket, conexion, i, j;
+    int net_socket, conexion, valread, i, j, k;
+    char buffer[8192] = { 0 };
 
-    char tVisible[17][18]={{' ','X','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','X'}, 
-					{'A','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-					{'B','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-					{'C','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-					{'D','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-					{'E','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-					{'F','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-					{'G','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-					{'H','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-					{'I','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-					{'J','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-					{'K','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-					{'L','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-					{'M','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-					{'N','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-					{'O','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-					{' ','X','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','X'}};
     public:
+    Tablero player;
     void CrearSocket(){
         net_socket = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -72,7 +58,7 @@ class Client{
         {   
             for (int j = 0; j < 18; j++)
             {
-                temp = tVisible[i][j];
+                temp = player.tVisible[i][j];
                 mensaje.append(temp);
             }
         }
@@ -82,7 +68,22 @@ class Client{
     }
 
     void Recibir(){
-        
+        valread = read(net_socket, buffer, 8192);
+    }
+
+    void ImprimirTableroDisparos(){
+        k=0;
+        printf(" \n");
+        for (size_t i = 0; i < 17; i++)
+        {
+            for (size_t j = 0; j < 18; j++)
+            {
+                printf("%3c", buffer[k]);
+                k++;
+            }
+            printf("\n");
+        }
+        printf("\n        1  2  3  4  5  6  7  8  9  10 11 12 13 14 15\n\n");
     }
 
     void CerrarSocket(){
@@ -91,80 +92,24 @@ class Client{
 };
 
 int main(){
+    
+    srand(time(NULL));
     Client cliente;
 
     cliente.CrearSocket();
     cliente.Conectar();
-    cliente.Enviar();
+
+    //Game Loop
+    while (1)
+    {
+        //server indica que es el turno del cliente para disparar
+        //cliente indica coordenadas, las envia
+        //server recibe coordenadas y envia tablero con resultados
+        //cliente indica que es el turno del server para disparar
+        //server indica coordenadas, las envia
+        //cliente recibe coordenadas y envia tablero con resultados
+    }
+
     cliente.CerrarSocket();
     return 0;
-    //string mensaje = "esteeselmensaje", temp;
-    //const char* mensaje_char = mensaje.c_str();
-    //int net_socket, i;
-
-    //socket protocolo TCP
-    // net_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-    // //inicializacion direccion IP y puerto
-    
-    // direccion_server.sin_family = AF_INET;
-    // direccion_server.sin_addr.s_addr = inet_addr(IP);
-    // direccion_server.sin_port = htons(PUERTO);
-
-    // //inicializar socket
-    // conexion = connect(net_socket,(struct sockaddr*)&direccion_server,sizeof(direccion_server));
-    
-    //revisar error de conexion
-
-    // if(conexion < 0){
-    //     cout << "ERROR EN LA CONEXION" << endl;
-    //     return 0;
-    // }
-
-    // cout << "CONEXION EXITOSA" << endl;
-
-    // char tVisible[17][18]={{' ','X','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','X'}, 
-	// 				{'A','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-	// 				{'B','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-	// 				{'C','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-	// 				{'D','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-	// 				{'E','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-	// 				{'F','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-	// 				{'G','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-	// 				{'H','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-	// 				{'I','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-	// 				{'J','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-	// 				{'K','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-	// 				{'L','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-	// 				{'M','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-	// 				{'N','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-	// 				{'O','|','~','~','~','~','~','~','~','~','~','~','~','~','~','~','~','|'},
-	// 				{' ','X','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','X'}};
-
-
-    //Formateo de mensaje
-    // mensaje.clear();
-    // for (int i = 0; i < 17; i++)
-    // {   
-    //     for (int j = 0; j < 18; j++)
-    //     {
-    //         temp = tVisible[i][j];
-    //         mensaje.append(temp);
-    //     }
-    // }
-    
-    // const char* mensaje_char = mensaje.c_str();
-    // send(net_socket, mensaje_char, mensaje.length(), 0);
-
-
-    //send(net_socket, tVisible, sizeof(tVisible), 0);
-
-    // while (1)
-    // {
-    //     cout << "Ingrese mensaje: ";
-    //     cin >> mensaje;
-    //     const char* mensaje_char = mensaje.c_str();
-    //     send(net_socket, mensaje_char, mensaje.length(), 0);
-    // }
-    
 }
--- neoServ.cpp
+++ neoServ.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <iostream>
+#include "Tablero.hpp"
 
 // inet_addr
 #include <arpa/inet.h>
@@ -21,12 +22,13 @@
 using namespace std;
 
 class Server{
-    
+    std::string mensaje,temp;
     int server_socket, new_socket, mensaje_recibido, valread, k;
     struct sockaddr_in direccion_server;
     struct sockaddr_storage serverStorage;
     socklen_t addr_size;
     char buffer[8192] = { 0 };
+    Tablero cpu;
 
     public:
     void CrearSocket(){
@@ -52,7 +54,19 @@ class Server{
     }
 
     void Enviar(){
-
+        //Formateo de mensaje
+        mensaje.clear();
+        for (int i = 0; i < 17; i++)
+        {   
+            for (int j = 0; j < 18; j++)
+            {
+                temp = cpu.tVisible[i][j];
+                mensaje.append(temp);
+            }
+        }
+        
+        const char* mensaje_char = mensaje.c_str();
+        send(new_socket, mensaje_char, mensaje.length(), 0);
     }
 
     void Recibir(){
@@ -82,59 +96,22 @@ class Server{
 
 int main(){
 
+    srand(time(NULL));
     Server servidor;
+
     servidor.CrearSocket();
-    servidor.Recibir();
-    servidor.ImprimirTableroDisparos();
-    servidor.CerrarSocket();
-    // int server_socket, new_socket, mensaje_recibido, valread, k;
-    // struct sockaddr_in direccion_server;
-    // struct sockaddr_storage serverStorage;
-    // socklen_t addr_size;
-    // char buffer[8192] = { 0 };
 
-    // server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	// direccion_server.sin_addr.s_addr = INADDR_ANY;
-	// direccion_server.sin_family = AF_INET;
-	// direccion_server.sin_port = htons(PUERTO);
-
-    // bind(server_socket,(struct sockaddr*)&direccion_server,sizeof(direccion_server));
-
-    // //escuchando equipos entrantes
-    // if (listen(server_socket, 3) == 0)
-	// 	printf("Listening\n");
-	// else
-	// 	printf("Error\n");
-
-    // addr_size = sizeof(serverStorage);
-    // new_socket = accept(server_socket,(struct sockaddr*)&direccion_server,&addr_size);
-    // if(new_socket <0) {
-    //     perror("accept");
-    //     exit(EXIT_FAILURE);
-    // }
-
-
-    // while (1)
-    // {
-    //         valread = read(new_socket, buffer, 8192);
-    //         cout << "Mensaje Entrante "<< buffer << endl;
-    // }
-
-
-    // k=0;
-    // printf(" \n");
-    // for (size_t i = 0; i < 17; i++)
-    // {
-    //     for (size_t j = 0; j < 18; j++)
-    //     {
-    //         printf("%3c", buffer[k]);
-    //         k++;
-    //     }
-    // }
-    // printf("\n        1  2  3  4  5  6  7  8  9  10 11 12 13 14 15\n\n");
+    //Game Loop
+    while (1)
+    {
+        //server indica que es el turno del cliente para disparar
+        //cliente indica coordenadas, las envia
+        //server recibe coordenadas y envia tablero con resultados
+        //cliente indica que es el turno del server para disparar
+        //server indica coordenadas, las envia
+        //cliente recibe coordenadas y envia tablero con resultados
+    }
     
-
-    // close(new_socket);
-    // shutdown(server_socket, SHUT_RDWR);
+    servidor.CerrarSocket();
     return 0;
 }

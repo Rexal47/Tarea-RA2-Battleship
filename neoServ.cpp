9be22a7c4a7a8fdfc4857f0680404a2786a6c5c4
Felipe Vásquez <minelyne@gmail.com>
2023-05-28 21:35:36 -0400

    elimacion codigo innecesario y nuevos comentarios

--- neoServ.cpp
+++ neoServ.cpp
@@ -31,6 +31,7 @@ class Server{
 
     public:
     void CrearSocket(){
+        //inicializacion direccion IP y puerto
         server_socket = socket(AF_INET, SOCK_STREAM, 0);
 	    direccion_server.sin_addr.s_addr = INADDR_ANY;
 	    direccion_server.sin_family = AF_INET;
@@ -38,20 +39,23 @@ class Server{
 
         bind(server_socket,(struct sockaddr*)&direccion_server,sizeof(direccion_server));
 
-        //escuchando equipos entrantes
+        //escuchando clientes entrantes
         if (listen(server_socket, 3) == 0)
             printf("Listening\n");
         else
             printf("Error\n");
 
         addr_size = sizeof(serverStorage);
+        //Se acepta cliente
         new_socket = accept(server_socket,(struct sockaddr*)&direccion_server,&addr_size);
         if(new_socket <0) {
             perror("accept");
             exit(EXIT_FAILURE);
         }
+        cout << "NUEVA CONEXION" << endl;
     }
 
+    //Envia informacion de juego
     void Enviar(Tablero jug, Tablero srv, bool turnoJugador){
         //Formateo de mensaje
         mensaje.clear();
@@ -96,25 +100,12 @@ class Server{
         send(new_socket, mensaje_char, mensaje.length(), 0);
     }
 
+    //Recibe informacion del cliente
     void Recibir(){
         valread = read(new_socket, buffer, 8192);
     }
 
-    void ImprimirTableroDisparos(){
-        k=0;
-        printf(" \n");
-        for (size_t i = 0; i < 17; i++)
-        {
-            for (size_t j = 0; j < 18; j++)
-            {
-                printf("%3c", buffer[k]);
-                k++;
-            }
-            printf("\n");
-        }
-        printf("\n        1  2  3  4  5  6  7  8  9  10 11 12 13 14 15\n\n");
-    }
-
+    //Se cierra conexion con cliente
     void CerrarSocket(){
         close(new_socket);
         shutdown(server_socket, SHUT_RDWR);
@@ -178,23 +169,17 @@ class Server{
 
 int main(){
 
+    //Creacion de instancia servidor
     srand(time(NULL));
     Server servidor;
 
+    //Se crea el socket de conexion
     servidor.CrearSocket();
 
+    //Se inicia el loop de juego principal
     servidor.juego();
 
-    //Game Loop
-    // while (1)
-    // {
-    //     //server indica que es el turno del cliente para disparar
-    //     //cliente indica coordenadas, las envia
-    //     //server recibe coordenadas y envia tablero con resultados
-    //     //cliente indica que es el turno del server para disparar
-    //     //server indica coordenadas, las envia
-    //     //cliente recibe coordenadas y envia tablero con resultados
-    // }
+    cout << "CONEXION CERRADA" << endl;
     
     servidor.CerrarSocket();
     return 0;
